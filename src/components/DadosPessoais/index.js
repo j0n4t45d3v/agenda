906e691e1c264ee5b7b8e8f64020b8c828e2d83d
Aluno <aluno@naverecife.local>
2023-02-13 21:38:47 -0300

    ajuste

---
+++ src/components/DadosPessoais/index.js
@@ -0,0 +1,11 @@
+import "./index.css";
+import icon from "../../assets/download.png";
+
+export function Titulo() {
+  return (
+    <div>
+      <h1>Dados Pessoais</h1>;
+      <img className="bolsonaro-dancarino" src={icon} />
+    </div>
+  );
+}
